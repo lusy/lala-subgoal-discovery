c0d92fc167527d92058d4321be8df222846c46a3
lusy <vaseva@mi.fu-berlin.de>
2011-11-13 18:52:04 +0100

    environment is generated with random start and goal

--- src/subgoal/rl.py
+++ src/subgoal/rl.py
@@ -2,8 +2,10 @@
 2 room 7x5 grid world."""
 
 from scipy import *
+from random import *
 
 from pybrain.rl.environments.mazes import Maze, MDPMazeTask
+#from pybrain.rl.environments.mazes.tasks.pomdp import POMDPTask
 from pybrain.rl.learners.valuebased import ActionValueTable
 from pybrain.rl.agents import LearningAgent
 from pybrain.rl.learners import Q
@@ -12,13 +14,11 @@ from pybrain.rl.experiments import Experiment
 #Create 2d 2room gridworld
 structure = array([[1,1,1,1,1,1,1],
                    [1,0,0,1,0,0,1],
-                   [1,0,0,1,0,0,1],
                    [1,0,0,0,0,0,1],
                    [1,0,0,1,0,0,1],
                    [1,0,0,1,0,0,1],
                    [1,1,1,1,1,1,1]])
 
-environment = Maze(structure, (1,5))
 
 #Initialize agent doing Q-Learning
 
@@ -28,11 +28,22 @@ controller.initialize(0.)
 learner = Q()
 agent = LearningAgent(controller, learner)
 
-#Create link between agent and environment
-task = MDPMazeTask(environment)
-experiment = Experiment(task, agent)
 
 while True:
+    #place random goal for each walk
+    [i,j] = structure.shape
+    goal = (randint(0,i-1),randint(0,j-1))
+
+    #place the goal in a field which is not a wall
+    while structure[goal] != 0:
+        goal = (randint(0,i-1),randint(0,j-1))
+
+    environment = Maze(structure, goal)
+
+    #Create link between agent and environment
+    task = MDPMazeTask(environment)
+    experiment = Experiment(task, agent)
+
     print "Starting..."
     print environment
     experiment.doInteractions(100)
